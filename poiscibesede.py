cae0c962ced03f271bcb64bc01086ec18e74198f
mariijajaneva <31701126+mariijajaneva@users.noreply.github.com>
2017-09-06 19:33:36 +0200

    Update poiscibesede.py

--- poiscibesede.py
+++ poiscibesede.py
@@ -1,3 +1,22 @@
+import time
 
+
+class PoisciBesede:
+  
+  def __init__(self, visina, sirina):
+    self.visina = visina
+    self.sirina = sirina
+    print('Ustvarili smo začetno ploščo')
+    
+  def vrstica(self, index):
+    
+    
+  def stolpec(self, index):
+    
+    
+    
+    
+  def kvadratek(self, index):
+    
 
 
