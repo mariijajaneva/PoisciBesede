df7ca6cadeacb548d2447255ea424cca619ba062
mariijajaneva <31701126+mariijajaneva@users.noreply.github.com>
2017-09-06 19:52:54 +0200

    Update poiscibesede.py

--- poiscibesede.py
+++ poiscibesede.py
@@ -9,9 +9,19 @@ class PoisciBesede:
     print('Ustvarili smo začetno ploščo')
     
   def vrstica(self, index):
+    vrstica = 0
+    for i in range(1,11):
+      vrstica += (self.visina) // 10
+      print ('vrstica')
+      return vrstica
+      
+      
     
     
   def stolpec(self, index):
+    for j in range(1, 13):
+      
+      
     
     
     
