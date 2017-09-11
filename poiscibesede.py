cb36e5313559dfb964aae78eb599f1e7d6de752d
mariijajaneva <31701126+mariijajaneva@users.noreply.github.com>
2017-09-11 23:16:10 +0200

    Update poiscibesede.py

--- poiscibesede.py
+++ poiscibesede.py
@@ -1,4 +1,7 @@
 import time
+import string
+import random
+
 
 
 class PoisciBesede:
@@ -31,11 +34,37 @@ class PoisciBesede:
     
     
     
+  
+  def postavitev_besede(beseda, plosca):
+    beseda = random.choice([beseda, beseda[::-1]])
     
-  besede = ["LUNA", "SLIKE", "OMARA", "OKNO", "OSA", "OGLEDALO", "VREČKA", "SVINČNIK", "ZNAMKA", "KOLEDAR", 
+    d = random.choice([[1,0], [0,1], [1,1]])
+    if d[0] == 0:
+      xsize = sirina
+     else:
+      xsize = sirina - len(beseda)
+      
+    if d[1] == 0:
+      ysize = visina
+    else:
+      ysize = visina - len(beseda)
+      
+    x = random.randrange(0, xsize)
+    y = random.randrange(0, ysize)
+    
+    for i in xrange(0, len(beseda)):
+      plosca[y + d[1] * i][x + d[0] * i] = beseda[i]
+    return plosca
+  
+  plosca = [[random.choice(string.uppercase) for i in xrange(0,sirina)] for j in xrange(0, visina)]
+    
+  print "\n",join(map(lambda row: " ".join(row), plosca)]
+                  
+  beseda = ["LUNA", "SLIKE", "OMARA", "OKNO", "OSA", "OGLEDALO", "VREČKA", "SVINČNIK", "ZNAMKA", "KOLEDAR", 
             "FLAŠA", "ZVEZEK", "BATERIJA", "KEMIK", "RAČUNALNIK", "RADIRKA", "ČOKOLADA", "DOM", "ROKOVNIK", 
             "REKA", "VERIŽICA", "KRPA", "MIŠ", "ŠKARJE", "KLOVN", "VODA", "NAŠA", "LEN"]
+                
+                
+                
   
-    
-
 
